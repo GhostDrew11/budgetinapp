ef4753a3492c1d969b93d3b82bf170c036ac6779
GhostDrew11 <armelhell@gmail.com>
2021-08-17 10:06:01 -0400

    NavBar Switching

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
+
+// import { makeStyles } from "@material-ui/core/styles";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
+import Mainpane from './Components/Mainpane';
+import {BrowserRouter as Router ,Route, Switch } from "react-router-dom"; 
+import Account from "./Pages/Accounts";
+import Categories from "./Pages/Categories";
+import Dashboard from "./Pages/Dashboard";
+import Recurring from "./Pages/Recurring";
+import Transactions from "./Pages/Transactions";
+
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     width: 100,
+//     height: 100,
+//   },
+// }));
 
 function App() {
+
+  //const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="root"> 
+        <div className="header"> 
+          <Header></Header> 
+        </div>
+        <div className="sidebar">
+          <Sidebar></Sidebar>
+        </div>
+        <div className="mainpane">
+          <Switch>
+            <Route path="/" exact component={Mainpane} />
+            <Route path="/accounts" component={Account} />
+            <Route path="/categories" component={Categories} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/recurring" component={Recurring} />
+            <Route path="/transactions" component={Transactions} />
+          </Switch>
+        </div>
+        
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/Pages/Recurring.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Recurring() {
+    return (
+        <div>Recurring</div>
+    );
+
+}
+
+export default Recurring;
